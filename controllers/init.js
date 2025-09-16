677bedca8dfc9f440666a9095e176bccc700a5b6
meghana202006 <meghanarodd20@gmail.com>
2025-09-17 00:05:27 +0530

    Initialing Repository feature

--- controllers/init.js
+++ controllers/init.js
@@ -1,6 +1,28 @@
-function initRepo()
-{
-    console.log("InitiRepo called");
+// To handle file operations
+const fs = require('fs').promises;
+// To find the path of directories
+const path = require('path');
+
+
+
+async function initRepo()
+{   // repoPath is the path of the git folder being add to
+    const repoPath = path.resolve(process.cwd(),".apnaGit");
+    // commitsPath is the path of commits folder
+    const commitsPath = path.join(repoPath,"commits");
+    try
+    {
+        await fs.mkdir(repoPath,{recursive:true});
+        await fs.mkdir(commitsPath,{recursive:true});
+        await fs.writeFile(
+            path.join(repoPath,"config.json"),
+            JSON.stringify({bucket:process.env.S3_BUCKET})
+        )
+        console.log("Repository initialized successfully");
+    }catch(err)
+    {
+        console.log("Error in initializing the repository",err);
+    }
 }
 
 module.exports = {initRepo};
